5421fe92640901a3eff07b181c1ab6f0dc60c073
Luis Daniel Serrano Avendano <daserr1025@gmail.com>
2025-05-28 21:31:21 -0700

    Corregi errores y agregue .gitgnore

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-
 class Config:
     SECRET_KEY = 'clave_secreta'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://gamingza_hxf7_user:kBRts7kWvInlDHomyR6DN59VWhuLlkKC@dpg-d0rsn524d50c73b1955g-a.oregon-postgres.render.com/gamingza_hxf7'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
